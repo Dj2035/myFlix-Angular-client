6f3f10cd647686a8bbc4955659b0eb7023c19551
Dj2035 <dk.jude@yahoo.com>
2022-10-13 15:00:13 +0200

    Imported login and registration forms and added function for opening login and registration modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(
+    public dialog: MatDialog
+  ) { }
+
+  //Function that will open the dialog modal when the sign up button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // Assigning style to dialog box
+      width: '300px'
+    });
+  }
+
+  //Function that will open the dialog modal when the Login button is clicked
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Assigning style to dialog box
+      width: '300px'
+    });
+  }
+
 }
