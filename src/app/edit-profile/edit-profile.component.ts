a8d6ba32a9b57360c0c387145059feb11f5535c4
Dj2035 <dk.jude@yahoo.com>
2022-10-17 16:30:53 +0200

    Added edit profile view

---
+++ src/app/edit-profile/edit-profile.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service'
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.scss']
+})
+
+export class EditProfileComponent implements OnInit {
+  @Input() userData: any = {};
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<EditProfileComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  //Allows users to edit their info
+  editUser(): void {
+    this.fetchApiData.editUser(this.userData).subscribe((result) => {
+      this.dialogRef.close();
+      console.log(result);
+      this.snackBar.open('Profile successfully updated!', 'OK', {
+        duration: 2000,
+      });
+
+      // Logs out user if they update Username or Password to avoid errors
+      if (this.userData.Username || this.userData.Password) {
+        localStorage.clear();
+        this.router.navigate(['welcome']);
+        this.snackBar.open(
+          'Please login again with your new credentials',
+          'OK',
+          {
+            duration: 2000,
+          }
+        );
+      }
+    });
+  }
+
+}
