81bf25d76de8b1da2d1bd6de527f70b478d2c2cc
Dj2035 <dk.jude@yahoo.com>
2022-10-17 15:41:49 +0200

    Added methods for getting user favorite movies, adding movies to favorite, opening genre, director and synopsis dialog

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FetchApiDataService } from '../fetch-api-data.service'
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { GenreComponent } from '../genre/genre.component';
+import { DirectorComponent } from '../director/director.component';
+import { SynopsisComponent } from '../synopsis/synopsis.component';
+
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,20 +16,96 @@ import { FetchApiDataService } from '../fetch-api-data.service'
 
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
+  favoriteMovies: any[] = [];
 
   constructor(
-    public fetchApiData: FetchApiDataService
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
     this.getMovies();
+    this.getUserFavoriteMovies();
   }
 
+  //Gets list of movies from API call
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  //Gets user favorite movies from api call and sets the favorite movies 
+  //variable to return JSON file
+  getUserFavoriteMovies(): void{
+    this.fetchApiData.getUser().subscribe((resp: any) => {
+      this.favoriteMovies = resp.FavoriteMovies;
+      console.log(this.favoriteMovies);
+      return this.favoriteMovies;
+    });
+  }
+
+  // Adds a movie to the list of favorite movies via an API call
+  addToFavoriteMovies(id: string): void {
+    console.log(id);
+    if (this.favoriteMovies.includes(id)) {
+      this.snackBar.open(
+        'This movie already exists in your fovorite movies list', 'OK', {
+        duration: 2000,
+      });
+    } 
+    this.fetchApiData.addFavoriteMovie(id).subscribe((result) => {
+      console.log(result);
+      this.ngOnInit();
+    });
+  }
+
+  //Removes a movie from the list of favorite movies via an API call
+  removeFromFavoriteMovies(id: string): void {
+    console.log(id);
+    this.fetchApiData.removeFavoriteMovie(id).subscribe((result) => {
+      console.log(result);
+      this.ngOnInit();
+    });
+  }
+
+  //Opens genre dialog from GenreComponent to display genre info
+  openGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+      // Assign dialog width
+      width: '500px',
+    });
+  }
+
+  //Opens director dialog from DirectorComponent to display info on director
+  openDirectorDialog(name: string, bio: string, birthday: string): void {
+    this.dialog.open(DirectorComponent, {
+      data: {
+        Name: name,
+        Bio: bio,
+        Birth: birthday,
+      },
+      // Assign dialog width
+      width: '500px',
+    });
+  }
+
+  //Opens synopsis dialog from SynopsisComponent for displaying movie details
+  openSynopsisDialog(title: string, description: string): void {
+    this.dialog.open(SynopsisComponent, {
+      data: {
+        Title: title,
+        Description: description,
+      },
+      // Assign dialog width
+      width: '500px',
     });
   }
 
